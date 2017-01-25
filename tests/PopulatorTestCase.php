16fcfa8e15d4b8fa32f5ce32eef444f6013e3100
guidocella <guidocella91@gmail.com>
2017-01-25 13:54:22 +0100

    Remove redundant import

--- tests/PopulatorTestCase.php
+++ tests/PopulatorTestCase.php
@@ -5,7 +5,6 @@ namespace EloquentPopulator;
 use EloquentPopulator\Models\Video;
 use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Database\Eloquent\Relations\Relation;
-use EloquentPopulator\ClassFinder;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Foundation\Testing\TestCase;
 
